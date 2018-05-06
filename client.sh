d4dc6c7a5e1bb4475bcf44a08d7d63faf156ade0
Jiajie Chen <jiegec@qq.com>
2018-05-06 08:29:56 +0800

    Add GPLv3 License

--- client.sh
+++ client.sh
@@ -1,4 +1,22 @@
 #!/usr/bin/env bash
+# Copyright (C) 2018 Jiajie Chen
+# 
+# This file is part of screenmux.
+# 
+# screenmux is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# screenmux is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with screenmux.  If not, see <http://www.gnu.org/licenses/>.
+# 
+
 # ffmpeg -f alsa -i hw:0 -f x11grab -s `xdpyinfo | grep 'dimensions:'|awk '{print $2}'` -i $DISPLAY -preset ultrafast -vcodec libx264 -tune zerolatency http://$1/feed.ffm
 # trap 'kill -TERM $PID' TERM INT
 
