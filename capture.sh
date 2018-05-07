5e980adf490cf4785400c6a35419ad4d3377587e
Jiajie Chen <jiegec@qq.com>
2018-05-07 16:45:55 +0800

    Improve x11 auto detect on Archlinux with GNOME

--- capture.sh
+++ capture.sh
@@ -17,16 +17,31 @@
 # along with screenmux.  If not, see <http://www.gnu.org/licenses/>.
 # 
 
-if [ -z "$DISPLAY" ]; then
-    DISPLAY=$(w -hs | awk -v tty="$(cat /sys/class/tty/tty0/active)" '$2 == tty && $3 != "-" {print $3}')
-    USER=$(w -hs | awk -v tty="$(cat /sys/class/tty/tty0/active)" '$2 == tty && $3 != "-" {print $1}')
+DIMENSIONS=$(xdpyinfo | grep 'dimensions:' | awk '{print $2;exit}')
+
+# Debian
+if [ -z "$DIMENSIONS" ]; then
+    DISPLAY=$(w -hs | awk -v tty="$(cat /sys/class/tty/tty0/active)" '$2 == tty && $3 != "-" {print $3; exit}')
+    USER=$(w -hs | awk -v tty="$(cat /sys/class/tty/tty0/active)" '$2 == tty && $3 != "-" {print $1; exit}')
     eval XAUTHORITY=~$USER/.Xauthority
+    export DISPLAY
     export XAUTHORITY
+    DIMENSIONS=$(xdpyinfo | grep 'dimensions:' | awk '{print $2;exit}')
 fi
 
-if [ -z "$DISPLAY" ]; then
+# GDM + Archlinux
+if [ -z "$DIMENSIONS" ]; then
+    DISPLAY=$(w -hs | awk 'match($2, /:[0-9]+/) {print $2; exit}')
+    USER=$(w -hs | awk 'match($2, /:[0-9]+/) {print $1; exit}')
+    eval XAUTHORITY=/run/user/$(id -u $USER)/gdm/Xauthority
+    export DISPLAY
+    export XAUTHORITY
+    DIMENSIONS=$(xdpyinfo | grep 'dimensions:' | awk '{print $2;exit}')
+fi
+
+if [ -z "$DIMENSIONS" ]; then
     ffmpeg -f fbdev -i /dev/fb0 -framerate 1 -y -frames:v 1 capture.jpg
 else
-    ffmpeg -f x11grab -s `xdpyinfo | grep 'dimensions:'|awk '{print $2}'` -framerate 1 -i $DISPLAY -y -frames:v 1 capture.jpg
+    ffmpeg -f x11grab -s $DIMENSIONS -framerate 1 -i $DISPLAY -y -frames:v 1 capture.jpg
 fi
 
--- client.sh
+++ client.sh
@@ -17,18 +17,33 @@
 # along with screenmux.  If not, see <http://www.gnu.org/licenses/>.
 # 
 
-if [ -z "$DISPLAY" ]; then
-    DISPLAY=$(w -hs | awk -v tty="$(cat /sys/class/tty/tty0/active)" '$2 == tty && $3 != "-" {print $3}')
-    USER=$(w -hs | awk -v tty="$(cat /sys/class/tty/tty0/active)" '$2 == tty && $3 != "-" {print $1}')
+DIMENSIONS=$(xdpyinfo | grep 'dimensions:' | awk '{print $2;exit}')
+
+# Debian
+if [ -z "$DIMENSIONS" ]; then
+    DISPLAY=$(w -hs | awk -v tty="$(cat /sys/class/tty/tty0/active)" '$2 == tty && $3 != "-" {print $3; exit}')
+    USER=$(w -hs | awk -v tty="$(cat /sys/class/tty/tty0/active)" '$2 == tty && $3 != "-" {print $1; exit}')
     eval XAUTHORITY=~$USER/.Xauthority
+    export DISPLAY
     export XAUTHORITY
+    DIMENSIONS=$(xdpyinfo | grep 'dimensions:' | awk '{print $2;exit}')
+fi
+
+# GDM + Archlinux
+if [ -z "$DIMENSIONS" ]; then
+    DISPLAY=$(w -hs | awk 'match($2, /:[0-9]+/) {print $2; exit}')
+    USER=$(w -hs | awk 'match($2, /:[0-9]+/) {print $1; exit}')
+    eval XAUTHORITY=/run/user/$(id -u $USER)/gdm/Xauthority
+    export DISPLAY
+    export XAUTHORITY
+    DIMENSIONS=$(xdpyinfo | grep 'dimensions:' | awk '{print $2;exit}')
 fi
 
 while true; do
-    if [ -z "$DISPLAY" ]; then
+    if [ -z "$DIMENSIONS" ]; then
         ffmpeg -f fbdev -i /dev/fb0 $2 -f flv $1
     else
-        ffmpeg -f x11grab -s `xdpyinfo | grep 'dimensions:'|awk '{print $2}'` -i $DISPLAY $2 -f flv $1
+        ffmpeg -f x11grab -s $DIMENSIONS -i $DISPLAY $2 -f flv $1
     fi
 done;
 
